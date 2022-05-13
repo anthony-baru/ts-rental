8ebf272464595d1f142125b2df842e396c942eaf
anthony baru <anthonybaru@gmail.com>
2022-05-13 19:44:18 +0300

    self signed cert error fix

--- config/connection.ts
+++ config/connection.ts
@@ -19,7 +19,10 @@ let opts = {
         return filename.substring(0, filename.indexOf(".model")) === toCamelCase(member);
     },
     dialectOptions: {
-        ssl: true,
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        },
         useUTC: false,
         dateStrings: true,
         typeCast: function (field: any, next: any) { // for reading from database
