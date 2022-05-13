8352c21f4459ec97736fde16a090b089700113bc
anthony baru <anthonybaru@gmail.com>
2022-05-13 12:46:22 +0300

    removed dist from src

--- dist/controller/carRentalVendor.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function createVehicle(req, res) {
-}
-exports.default = {
-    createVehicle
-};
-//# sourceMappingURL=carRentalVendor.controller.js.map
--- dist/services/gis.service.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=gis.service.js.map
